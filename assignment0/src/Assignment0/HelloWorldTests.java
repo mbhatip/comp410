08dcf24aa70f660c452a63c0a3bf48dd6c73e8d6
Mehmet Hatip <mbhatip@gmail.com>
2020-01-20 17:54:25 -0500

    added assignment 0

---
+++ assignment0/src/Assignment0/HelloWorldTests.java
@@ -0,0 +1,21 @@
+package Assignment0;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HelloWorldTests {
+
+	HelloWorld helloworld = new HelloWorld();
+	
+	@Test
+	public void testSay_it() {
+		assertEquals("hello world", helloworld.say_it());
+	}
+
+	@Test
+	public void testSay_it_loud() {
+		assertEquals("HELLO WORLD", helloworld.say_it_loud());
+	}
+
+}
