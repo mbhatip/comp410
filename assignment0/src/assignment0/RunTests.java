6546c22a83828e94996fe7aa74b3dde06a3bd04f
memohat <memohat@localhost.localdomain>
2020-02-01 16:58:37 -0500

    removed testing

--- assignment0/src/assignment0/RunTests.java
+++
@@ -1,8 +0,0 @@
-package assignment0;
-import gradingTools.comp410s20.assignment0.testcases.Assignment0Suite;
-
-public class RunTests {
-  public static void main(String[] args){
-    Assignment0Suite.main(args);
-  }
-}
