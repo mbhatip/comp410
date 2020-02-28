86b5d58d6b3ef234d3f9151f421e09a0aa731b0d
Memohat <49382701+Memohat@users.noreply.github.com>
2020-02-28 13:47:32 -0500

    started a3

---
+++ assignment3/src/MaxBinHeap_A3/MaxBinHeap.java
@@ -0,0 +1,108 @@
+package MaxBinHeap_A3;
+
+public class MaxBinHeap implements Heap_Interface {
+	private double[] array; //load this array
+	private int size;
+	private static final int arraySize = 10000; //Everything in the array will initially 
+                                      //be null. This is ok! Just build out 
+                                      //from array[1]
+
+	public MaxBinHeap() {
+	    this.array = new double[arraySize];
+	    this.array[0] = Double.NaN;  //0th will be unused for simplicity 
+				                     //of child/parent computations...
+				                     //the book/animation page both do this.
+	    size = 0;
+	}
+	    
+	//Please do not remove or modify this method! Used to test your entire Heap.
+	@Override
+	public double[] getHeap() { 
+		return this.array;
+	}
+
+	@Override
+	public void insert(double element) {
+		// TODO Auto-generated method stub
+		bubbleDown(element, 1);
+		size++;
+	}
+	
+	@Override
+	public void delMax() {
+		// TODO Auto-generated method stub
+		if (size > 1) {
+			array[1] = 0;
+			bubbleDown(array[size], 1);
+			array[size] = 0;
+			size--;
+		}
+		else {
+			clear();
+		}
+	}
+	
+	private void bubbleDown(double val, int index) {
+		double left = array[index*2];
+		double right = array[index*2 + 1];
+		if (val > left && val > right) {
+			array[index] = val;
+		}
+		else if (left > right) {
+			array[index] = left;
+			bubbleDown(val, index*2);
+		}
+		else {
+			array[index] = right;
+			bubbleDown(val, index*2 + 1);
+		}
+	}
+	
+	private void bubbleDown(int index) {
+		bubbleDown(array[index], index);
+	}
+	
+	@Override
+	public double getMax() {
+		return size == 0 ? Double.NaN : array[1];
+	}
+	
+	@Override
+	public void clear() {
+		this.array = new double[arraySize];
+		this.array[0] = Double.NaN;
+		size = 0;
+	}
+	
+	@Override
+	public int size() {
+		return size;
+	}
+	
+	@Override
+	public void build(double[] elements) {
+		// TODO Auto-generated method stub
+		clear();
+		size = elements.length;
+		for (int i = 0; i < size; i++) {
+			this.array[i+1] = elements[i];
+		}
+		int index = size/2;
+		while (index >= 1) {
+			bubbleDown(index--);
+		}
+	}
+	
+	@Override
+	public double[] sort(double[] elements) {
+		build(elements.clone());
+		double sorted[] = elements.clone();
+		for (int i = sorted.length - 1; i >= 0; i--) {
+			sorted[i] = getMax();
+			delMax();
+		}
+		build(elements.clone());
+		return sorted;
+		
+	}
+}
